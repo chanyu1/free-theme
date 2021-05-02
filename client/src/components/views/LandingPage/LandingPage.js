b0ed02b9d5c28db66f97b1a5b868f75be22f3155
chanyu1 <haneul1ij@gmail.com>
2021-05-02 22:52:21 +0900

    Create logout page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,21 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
+
+  const logoutHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      // console.log(response.data);
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to logout.");
+      }
+    });
+  };
 
   return (
     <div
@@ -17,6 +28,7 @@ function LandingPage() {
       }}
     >
       <h2>시작페이지</h2>
+      <button onClick={logoutHandler}>로그아웃</button>
     </div>
   );
 }
