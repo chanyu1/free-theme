f0bd5011e8a12ca5c0ee6503e5a00d464bd9dfc0
chanyu1 <haneul1ij@gmail.com>
2022-10-17 20:12:27 +0900

    test commit

--- server/routes/postcardRoutes.js
+++ server/routes/postcardRoutes.js
@@ -1,5 +1,9 @@
+const path = require('path');
 const multer = require('multer');
-const upload = multer({ dest: '../client/public/uploads' });
+// const upload = multer({ dest: '../client/public/uploads' });
+const upload = multer({
+  dest: path.join(__dirname, '../../client', 'public', 'uploads'),
+});
 
 const fs = require('fs');
 const util = require('util');
